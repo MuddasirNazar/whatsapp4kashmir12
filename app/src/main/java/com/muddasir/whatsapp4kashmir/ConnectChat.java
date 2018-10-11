7f558937e12c5e9d9432090ed000d6e7b7a838e3
MUDDASIR <nazar4314@gmail.com>
2018-10-11 17:56:58 +0530

    commit testing new file

--- app/src/main/java/com/muddasir/whatsapp4kashmir/ConnectChat.java
+++ app/src/main/java/com/muddasir/whatsapp4kashmir/ConnectChat.java
@@ -1,4 +1,24 @@
 package com.muddasir.whatsapp4kashmir;
 
 public class ConnectChat {
+
+    String userId;
+
+    String  message;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
